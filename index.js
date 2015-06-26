660fd42e3176ea1d42e2e42ef91370874a28c65e
Stefan Penner <stefan.penner@gmail.com>
2015-06-26 13:25:02 -0700

    [fixes #4] auto-disable for ember 2.0

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+var VersionChecker = require('ember-cli-version-checker');
+
 module.exports = {
-  name: 'ember-cli-auto-register-helpers'
+  name: 'ember-cli-auto-register-helpers',
+
+  init: function() {
+    var checker = new VersionChecker(this);
+    this._checkerForEmber = checker.for('ember', 'bower');
+  },
+
+  treeFor: function() {
+    if (this._checkerForEmber.lt('2.0.0-beta.1')) {
+      return this._super.treeFor.apply(this, arguments);
+    } else {
+      console.warn('ember-auto-register-helpers is not required for Ember 2.0.0 and later please remove from your `package.json`.');
+    }
+  }
 };
